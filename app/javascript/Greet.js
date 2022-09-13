505a85dbf3560aa89e0dc1d1b0a407bfbccf2e0a
arturo <ahpareja@gmail.com>
2022-09-12 21:14:56 -0500

    Create random greeting

--- app/javascript/Greet.js
+++ app/javascript/Greet.js
@@ -1,2 +1,22 @@
 import react, { userEffect } from 'react';
 import { userDispatch, useSelector } from 'react-redux';
+import { fetchGreeting } from './redux/helloReducer';
+
+const Greet = () => {
+  const dispatch = userDispatch();
+  const { greeting } = useSelector((state) => state.hello);
+
+  userEffect(() => {
+    dispatch(fetchGreeting());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>{greeting}</h1>
+      {' '}
+      World!
+    </div>
+  );
+};
+
+export default Greet;
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,15 @@
+import '@hotwired/turbo-rails';
+import './controllers';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './App';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
